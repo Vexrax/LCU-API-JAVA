91cfd58d353fd819bbaeb477f1576a56cea80434
Vexrax <joshuaf_111@live.com>
2018-12-08 17:56:22 -0500

    Added some exceptions + basic APIRequest function

--- src/LCUConnector/LeagueClient.java
+++ src/LCUConnector/LeagueClient.java
@@ -1,6 +1,27 @@
 package LCUConnector;
 
+import com.sun.deploy.net.HttpResponse;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.concurrent.CompletableFuture;
+
 public class LeagueClient
 {
-    
+    public CompletableFuture<HttpResponse> MakeApiRequest(HttpMethod methodType, String endpoint, Object Data) throws RequestTypeNotFoundException
+    {
+        switch(methodType)
+        {
+            case Get:
+                return CompletableFuture.completedFuture(new HttpResponse);
+            case Put:
+                return CompletableFuture.completedFuture(new HttpResponse);
+            case Post:
+                return CompletableFuture.completedFuture(new HttpResponse);
+            case Delete:
+                return CompletableFuture.completedFuture(new HttpResponse);
+            default:
+                throw new RequestTypeNotFoundException(methodType);
+        }
+    }
+
 }
