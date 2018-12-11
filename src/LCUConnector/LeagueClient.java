13779a36ed44641ba5ff3c4eb06c633c16a179e2
Vexrax <joshuaf_111@live.com>
2018-12-10 21:32:28 -0500

    Connector Cleaned up and refacotred

--- src/LCUConnector/LeagueClient.java
+++ src/LCUConnector/LeagueClient.java
@@ -1,35 +1,25 @@
 package LCUConnector;
 
 
-import org.apache.commons.codec.Charsets;
 import org.apache.commons.codec.binary.Base64;
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.RequestDefaultHeaders;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.BasicResponseHandler;
+
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.CompletableFuture;
-
 
 
 public class LeagueClient
 {
+    String LeagueExecutablePath;
+
     String Token;
     String ApiUri;
     int Port;
@@ -39,17 +29,18 @@ public class LeagueClient
     byte[] EncodedAuth;
     HttpPost httpost;
     HttpGet httpget;
-
+    HttpPut httput;
+    HttpDelete httpdelete;
 
     /**
      * Used for Creating the connection if you know the location of the league of legends execuatble.
      * @param LeagueExecutablePath Path to the LeagueOfLegends executable
      */
-    public LeagueClient(String LeagueExecutablePath)
+    public LeagueClient(String leagueExecutablePath)
     {
         this.Client =  HttpClientBuilder.create().build();
-        GetLockFileCredintials("C:\\Riot Games\\League of Legends\\lockfile");
-        tempMakeReq();
+        this.LeagueExecutablePath = leagueExecutablePath;
+        //ExecuteGetRequest("lol-maps/v1/maps", new Object()); //Todo: Remove Later, this is used for testing
     }
 
     /**
@@ -60,62 +51,35 @@ public class LeagueClient
 
     }
 
-    public void tempMakeReq()
+    public void Connect()
     {
-        try
-        {
-            this.httpget =  new HttpGet(ApiUri + "lol-maps/v1/maps");
-            this.httpget.setHeader("Accept", "application/json");
-            httpget.setHeader("Content-type", "application/json");
-            httpget.setHeader("Authorization", "Basic " +  this.EncodedAuth);
-            HttpResponse response = this.Client.execute(httpget);
-             
-            HttpEntity entity = response.getEntity();
-            Header encodingHeader = entity.getContentEncoding();
-
-            // you need to know the encoding to parse correctly
-            Charset encoding = encodingHeader == null ? StandardCharsets.UTF_8 :
-                    Charsets.toCharset(encodingHeader.getValue());
-
-            // use org.apache.http.util.EntityUtils to read json as string
-            String json = EntityUtils.toString(entity, StandardCharsets.UTF_8);
-            System.out.println(json);
-        }
-
-        catch(Exception e)
-        {
-            System.out.println(e);
-        }
+        GetLockFileCredentials(LeagueExecutablePath);
     }
 
-    //public CompletableFuture<LeagueClient> Connect()
-    //{
-    //    return CompletableFuture();
-    //}
 
-
-//    public CompletableFuture<HttpResponse> MakeApiRequest(HttpMethod methodType, String endpoint, Object Data) throws RequestTypeNotFoundException
-//    {
-//        switch(methodType)
-//        {
-//            case Get:
-//                return CompletableFuture(new HttpResponse);
-//            case Put:
-//                return CompletableFuture(new HttpResponse);
-//            case Post:
-//                return CompletableFuture(new HttpResponse);
-//            case Delete:
-//                return CompletableFuture(new HttpResponse);
-//            default:
-//                throw new RequestTypeNotFoundException(methodType);
-//        }
-//    }
+    public HttpResponse MakeApiRequest(HttpMethod methodType, String endpoint, Object Data) throws RequestTypeNotFoundException
+    {
+        switch(methodType)
+        {
+            case Get:
+                return ExecuteGetRequest(endpoint, Data);
+            case Put:
+                return ExecutePutRequest(endpoint,Data);
+            case Post:
+                return ExecutePostRequest(endpoint,Data);
+            case Delete:
+                return ExecuteDeleteRequest(endpoint,Data);
+            default:
+                throw new RequestTypeNotFoundException(methodType);
+        }
+    }
 
     public void Disconnect()
     {
-
+        //todo
     }
-    private void GetLockFileCredintials(String PathToLockFile)
+
+    private void GetLockFileCredentials(String PathToLockFile)
     {
         BufferedReader reader;
         try
@@ -128,14 +92,9 @@ public class LeagueClient
             this.ApiUri = "https://127.0.0.1:" + this.Port + "/";
             this.LeaguePID = Integer.parseInt(items[1]);
 
-            System.out.println(this.Token);
-            System.out.println(this.Port);
-            System.out.println(this.ApiUri);
-            System.out.println(this.LeaguePID);
 
-            String auth = "riot" + ":" + this.Token;
+            String auth = "riot:" + this.Token;
             this.EncodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-
 
         }
         catch (Exception e)
@@ -143,5 +102,107 @@ public class LeagueClient
             System.out.println(e);
         }
     }
+
+    private HttpResponse ExecuteGetRequest(String endPoint, Object data)
+    {
+        try
+        {
+            this.httpget =  new HttpGet(ApiUri + endPoint);
+            this.httpget.setHeader("Accept", "application/json");
+
+            //todo Add a way to pass data into the LCU
+
+            httpget.setHeader("Content-type", "application/json");
+            httpget.setHeader("Authorization", "Basic " +  new String(this.EncodedAuth));
+            HttpResponse response = this.Client.execute(httpget);
+//            HttpEntity entity = response.getEntity();
+//            Header encodingHeader = entity.getContentEncoding();
+//
+//            // you need to know the encoding to parse correctly
+//            Charset encoding = encodingHeader == null ? StandardCharsets.UTF_8 :
+//                    Charsets.toCharset(encodingHeader.getValue());
+//
+//            // use org.apache.http.util.EntityUtils to read json as string
+//            String json = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+//            return json; //todo Change this to a JSON
+            return response;
+        }
+
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    private HttpResponse ExecutePostRequest(String endPoint, Object data)
+    {
+        try
+        {
+            this.httpost =  new HttpPost(ApiUri + endPoint);
+            this.httpost.setHeader("Accept", "application/json");
+
+            //todo Add a way to pass data into the LCU
+
+            httpost.setHeader("Content-type", "application/json");
+            httpost.setHeader("Authorization", "Basic " +  new String(this.EncodedAuth));
+            HttpResponse response = this.Client.execute(httpget);
+
+            return response;
+        }
+
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    private HttpResponse ExecutePutRequest(String endPoint, Object data)
+    {
+        try
+        {
+            this.httput =  new HttpPut(ApiUri + endPoint);
+            this.httput.setHeader("Accept", "application/json");
+
+            //todo Add a way to pass data into the LCU
+
+            httput.setHeader("Content-type", "application/json");
+            httput.setHeader("Authorization", "Basic " +  new String(this.EncodedAuth));
+            HttpResponse response = this.Client.execute(httpget);
+
+            return response;
+        }
+
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    private HttpResponse ExecuteDeleteRequest(String endPoint, Object data)
+    {
+        try
+        {
+            this.httpdelete =  new HttpDelete(ApiUri + endPoint);
+            this.httpdelete.setHeader("Accept", "application/json");
+
+            //todo Add a way to pass data into the LCU
+
+            httpdelete.setHeader("Content-type", "application/json");
+            httpdelete.setHeader("Authorization", "Basic " +  new String(this.EncodedAuth));
+            HttpResponse response = this.Client.execute(httpget);
+
+            return response;
+        }
+
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return null;
+    }
+
 
 }
