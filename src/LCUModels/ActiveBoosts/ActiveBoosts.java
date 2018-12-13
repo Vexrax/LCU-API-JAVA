414a7554c8cc7a9b90280bf4a31ee969cfb4f086
Vexrax <joshuaf_111@live.com>
2018-12-12 19:58:22 -0500

    Adding active boost stubs

---
+++ src/LCUModels/ActiveBoosts/ActiveBoosts.java
@@ -0,0 +1,12 @@
+package LCUModels.ActiveBoosts;
+
+public class ActiveBoosts
+{
+    String firstWinOfTheDayStartTime;
+    String ipBoostEndDate;
+    int ipBoostPerWinCount;
+    int summonerId;
+    String xpBoostEndDate;
+    int xpBoostPerWinCount;
+    int xpLoyaltyBoost;
+}
